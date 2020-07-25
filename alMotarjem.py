02a85f82da12b2c73cb8337ac6cc6d498e662951
MohamedBdeir <mohamad.bdeir@isae.edu.lb>
2020-07-25 19:43:28 +0300

    added sub and fixed requirements

--- alMotarjem.py
+++ alMotarjem.py
@@ -22,14 +22,14 @@ reddit = praw.Reddit(client_id='2Qv7SGX2aP_3_A',
                     username='alMotarjem',
                     password='tarjem31387875244*')
 
-subredditsArray = ['AlMotarjem']
+subredditsArray = ['AlMotarjem', 'lebanese', 'lebanon']
 
 for sub in subredditsArray:
     subreddit = reddit.subreddit(sub)
     for comment in subreddit.comments(limit=20):
-        print(comment.id)
         commentBody = comment.body
         if('!tarjem' in commentBody and comment.id not in replied):
+            print(commentBody)
             if( type(comment.parent()) is praw.models.reddit.submission.Submission):
                submissionTitle = comment.parent().title
                submissionText = comment.parent().selftext
@@ -38,23 +38,20 @@ for sub in subredditsArray:
                if postLanguage.lang =='ar' or titleLanguage.lang =='ar':
                    titleTranslation = translator.translate(submissionTitle).text
                    postTranslation = translator.translate(submissionText).text
-
                    titleReply = "Title: "+titleTranslation+"\n \n"
                    bodyReply = "Body: "+postTranslation+"\n \n"
                    finalReply = titleReply+" "+bodyReply+" \n \n ^(I am a bot, I work using google translate)"
-                   comment.reply(finalReply)
-                   id = comment.id
+                   comment.reply(finalReply)                  
                else: 
                    print("I only translate from english to arabic ^(I am a bot, I work using google translate)")
-               firebase.post('/almotarjem-eca52/', id)
             else:
                 commentBody = comment.body
                 text = comment.parent().body
-                print(text)
                 language = translator.detect(text)
                 if language.lang =='ar':
                     translatedText = translator.translate(text).text
                     comment.reply(translatedText+" \n \n ^(I am a bot, I work using google translate)")
                 else:
                     comment.reply("I can only translate from arabic to english \n \n ^(I am a bot, I work using google translate")
-                firebase.post('/almotarjem-eca52/', comment.id)
+            firebase.post('/almotarjem-eca52/', comment.id)           
+        
